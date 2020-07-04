1616fe9c5fdb2cad63fc94de38cd28d56f51c59a
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-07-04 08:44:32 +0530

    added http to https redirection

--- server/index.js
+++ server/index.js
@@ -4,16 +4,26 @@ import fs from "fs";
 import React from "react";
 import express from "express";
 import compression from "compression";
-import helmet from 'helmet';
+import helmet from "helmet";
 import ReactDOMServer from "react-dom/server";
 
 import App from "../src/App";
 
 const PORT = process.env.PORT || 3006;
 const app = express();
-app.use(helmet())
+app.use(helmet());
 app.use(express.static("./build"));
 app.use(compression());
+
+app.use(function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
 
 app.get("/*", (req, res) => {
   const app = ReactDOMServer.renderToNodeStream(<App />);
@@ -25,7 +35,7 @@ app.get("/*", (req, res) => {
       return res.status(500).send("Oops, better luck next time!");
     }
 
-    res.set('Content-Encoding', 'gzip')
+    res.set("Content-Encoding", "gzip");
     return res.send(
       data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
     );

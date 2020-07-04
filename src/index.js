0a2d63dbb780bc5d954ae06bb64d8da6dedbd4a7
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-07-04 19:21:35 +0530

    fetched page 0 data and used to hydrate.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import getNews from "./utils/getNews";
 
-ReactDOM.hydrate(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+getNews(0)
+  .then((res) => {
+    ReactDOM.hydrate(
+      <React.StrictMode>
+        <App data={res} isLineChartVisible={true} />
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

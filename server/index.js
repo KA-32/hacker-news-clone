3cfd703c07b8f3ceb04e6a8a3f42f38ba1e7c74e
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-07-04 07:56:33 +0530

    added helmet and compression packages for optimization

--- server/index.js
+++ server/index.js
@@ -3,17 +3,20 @@ import fs from "fs";
 
 import React from "react";
 import express from "express";
+import compression from "compression";
+import helmet from 'helmet';
 import ReactDOMServer from "react-dom/server";
 
 import App from "../src/App";
 
 const PORT = process.env.PORT || 3006;
 const app = express();
-
+app.use(helmet())
 app.use(express.static("./build"));
+app.use(compression());
 
 app.get("/*", (req, res) => {
-  const app = ReactDOMServer.renderToString(<App />);
+  const app = ReactDOMServer.renderToNodeStream(<App />);
 
   const indexFile = path.resolve("./build/index.html");
   fs.readFile(indexFile, "utf8", (err, data) => {
@@ -22,6 +25,7 @@ app.get("/*", (req, res) => {
       return res.status(500).send("Oops, better luck next time!");
     }
 
+    res.set('Content-Encoding', 'gzip')
     return res.send(
       data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
     );
